cdf38b3ba85d0be02a9900359b4ad374465dd2ac
Nuria O <nuria.oyaga@gmail.com>
2015-10-19 14:48:24 +0200

    Ejercicio 6 con error en to_json

--- karaoke.py
+++ karaoke.py
@@ -9,6 +9,44 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
+class KaraokeLocal(SmallSMILHandler):
+    """Clase de Karaoke """
+    def __init__(self, fichero):
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(fichero)
+        self.datos = cHandler.get_tags()
+
+    def __str__(self):
+        linea = ""
+        for elem in self.datos:
+            linea = linea + elem[0]
+            atributos = elem[1].items()
+            for nombre, valor in atributos:
+                linea = linea + '\t' + nombre + ' = ' + '"' + valor + '"'
+            linea = linea + '\n'
+        return linea
+
+    def to_json(self, fichsmil, new=''):
+        """if new == '':
+            new = fichsmil.split('.')[0] + '.json'"""
+        with open(new, 'w') as fichero_json:
+            json.dump(self.datos, fichero_json, sort_keys=True, indent=4, separators=(' ', ': '))
+
+    def do_local(self):
+        for elem in self.datos:
+            atributos = elem[1]
+            try:
+                url = atributos['src']
+                if url != "cancion.ogg":
+                    filename = url[url.rfind("/") + 1:]
+                    data = urllib.request.urlretrieve(url, filename)
+                    atributos['src'] = "http://" + data[0]
+            except KeyError as e:
+                pass
+
+
 def abrirfichero():
     try:
         fich = open(sys.argv[1], 'r')
@@ -19,47 +57,11 @@ def abrirfichero():
         sys.exit("Not file with this name")
 
 
-def get_list(fich):
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(fich)
-    list = cHandler.get_tags()
-    return list
-
-
-def print_list(lista):
-    linea = ""
-    for elem in lista:
-        linea = linea + elem[0]
-        atributos = elem[1].items()
-        for nombre, valor in atributos:
-            linea = linea + '\t' + nombre + ' = ' + '"' + valor + '"'
-        linea = linea + '\n'
-    print (linea)
-
-
-def to_json(list):
-    with open('karaoke.json', 'w') as fichero_json:
-        json.dump(list, fichero_json, sort_keys=True, indent=4, separators=(' ', ': '))
-
-
-def do_local(list):
-    for elem in list:
-        atributos = elem[1]
-        try:
-            url = atributos['src']
-            if url != "cancion.ogg":
-                filename = url[url.rfind("/") + 1:]
-                data = urllib.request.urlretrieve(url, filename)
-                atributos['src'] = "http://" + data[0]
-        except KeyError as e:
-            pass
-
-
 if __name__ == '__main__':
     fichero = abrirfichero()
-    lista = get_list(fichero)
-    print_list(lista)
-    to_json(lista)
-    do_local(lista)
+    print(fichero)
+    karaoke = KaraokeLocal(fichero)
+    print(karaoke)
+    #karaoke.to_json(fichero)
+    karaoke.do_local()
+    karaoke.to_json(fichero, 'local.json')
